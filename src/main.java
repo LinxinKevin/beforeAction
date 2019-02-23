12eb3d62367b1c2bb915e37257356caef50dc42a
linxinkevin@gmail.com <oliver712>
2019-02-23 19:57:54 +0800

    second commit - add the second line

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("this is first line");
+        System.out.println("this second line is appending now");
     }
 }
